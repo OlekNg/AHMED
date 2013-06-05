6c1359752194a4831399f391b8f7b2dc96b22031
Andrzej Bartoszek <andrzej.tg.bartoszek@gmail.com>
2013-06-05 18:30:46 +0200

    Added essential properties to simulator

--- Simulation/Simulator.cs
+++ Simulation/Simulator.cs
@@ -9,7 +9,11 @@ namespace Simulation
 {
     public class Simulator
     {
+        public BuildingMap BuildingMap { get; set; }
 
+        public PeopleMap PeopleMap { get; set; }
+
+        public int MaximumTicks { get; set; }
 
         public PeopleGroup[] Simulate(bool[] genotype)
         {
