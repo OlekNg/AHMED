2247712c3d992236247394f1472df891fbc808c0
Andrzej Bartoszek <andrzej.tg.bartoszek@gmail.com>
2013-06-06 19:46:56 +0200

    BuildingMap refactoring and method to setup map

--- Structure/BuildingMap.cs
+++ Structure/BuildingMap.cs
@@ -12,10 +12,58 @@ namespace Structure
 
         public uint Width { get; set; }
 
-        public Door[] Doors { get; set; }
+        //public Door[] Doors { get; set; }
 
-        public Wall[] Walls { get; set; }
+        //public Wall[] Walls { get; set; }
 
         public FloorSquare[][] Floor { get; set; }
+
+        public void SetSize(uint h, uint w)
+        {
+            Height = h;
+            Width = w;
+            Floor = new FloorSquare[w][];
+            for (int i = 0; i < h; ++i)
+            {
+                Floor[i] = new FloorSquare[h];
+            }
+        }
+
+        public void SetFloor(uint x, uint y, uint capacity)
+        {
+            Floor[x][y] = new FloorSquare(capacity);
+        }
+
+        public void SetWall(uint x, uint y)
+        {
+            SetWallElement(x, y, new Wall());
+        }
+
+        public void SetDoor(uint x, uint y, uint capacity)
+        {
+            SetWallElement(x, y, new Door { Capacity = capacity });
+        }
+
+        private void SetWallElement(uint x, uint y, IWallElement wallElement)
+        {
+            if (x % 2 == 0)
+            {
+                //column walls
+                if (x == 0)
+                {
+                    //first column
+                    
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                //row walls
+
+            }
+        }
     }
 }
