a44a7a7ba292c0ac3f4032899f7d6fdf1a45f946
Andrzej Bartoszek <andrzej.tg.bartoszek@gmail.com>
2013-06-06 16:45:34 +0200

    Added Escaped Group (to return values from simulator)

---
+++ Simulation/EscapedGroup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation
+{
+    public class EscapedGroup
+    {
+        public uint Quantity { get; set; }
+
+        public uint Ticks { get; set; }
+
+        public EscapedGroup(EvacuationElement ee)
+        {
+            Quantity = ee.PeopleQuantity;
+            Ticks = ee.Ticks;
+        }
+    }
+}
