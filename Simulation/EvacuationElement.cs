bcf179464e6ad804dfac96f2c9b96d4296e77661
Andrzej Bartoszek <andrzej.tg.bartoszek@gmail.com>
2013-06-06 16:45:02 +0200

    Moved some info from EvacGroup to EvacElement (and removed EvacGroup)

--- Simulation/EvacuationElement.cs
+++ Simulation/EvacuationElement.cs
@@ -11,10 +11,21 @@ namespace Simulation
     {
         public FloorSquare FloorSquare { get; set; }
 
-        public EvacuationElement NextStep { get; set; }
-
         public IWallElement Passage { get; set; }
 
-        public EvacuationGroup EvacuatingGroup { get; set; }
+        public uint PeopleQuantity { get; set; }
+
+        public uint Ticks { get; set; }
+
+        public bool Processed { get; set; }
+
+        public EvacuationElement NextStep { get; set; }
+
+        public uint PeopleQuantityLeft { 
+            get 
+            { 
+                return FloorSquare.Capacity - PeopleQuantity; 
+            } 
+        }
     }
 }
