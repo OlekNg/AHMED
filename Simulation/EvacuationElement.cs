530ed58bbaf62877954700669dda9b5f910c387c
Andrzej Bartoszek <andrzej.tg.bartoszek@gmail.com>
2013-06-06 15:01:53 +0200

    Added EvacuationElement/Group

---
+++ Simulation/EvacuationElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Structure;
+
+namespace Simulation
+{
+    public class EvacuationElement
+    {
+        public FloorSquare FloorSquare { get; set; }
+
+        public EvacuationElement NextStep { get; set; }
+
+        public IWallElement Passage { get; set; }
+
+        public EvacuationGroup EvacuatingGroup { get; set; }
+    }
+}
