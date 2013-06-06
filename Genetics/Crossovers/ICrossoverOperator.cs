13672a94349c12e81abc919410a0f2880eddc622
Aleksander Szczepanek <aleksander.szczepanek@gmail.com>
2013-06-06 12:57:25 +0200

    Added interface for crossing over operator.
    Added CrossoverException.
    Implemented one point crossover.

---
+++ Genetics/Crossovers/ICrossoverOperator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetics.Crossovers
+{
+    /// <summary>
+    /// Interface for crossover operators.
+    /// </summary>
+    interface ICrossoverOperator
+    {
+        /// <summary>
+        /// Performs crossover. Doesn't affect arguments.
+        /// </summary>
+        /// <param name="c1">First chromosome.</param>
+        /// <param name="c2">Second chromosome.</param>
+        /// <returns>Two new chromosomes.</returns>
+        Chromosome[] CrossAndCreate(Chromosome c1, Chromosome c2);
+
+        /// <summary>
+        /// Performs crossover replacing given chromosomes.
+        /// </summary>
+        /// <param name="c1">First chromosome.</param>
+        /// <param name="c2">Second chromosome.</param>
+        void CrossAndReplace(Chromosome c1, Chromosome c2);
+    }
+}
