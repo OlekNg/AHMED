7e082c854ed72c201f97c18f1a20ac690dccc45f
Aleksander Szczepanek <aleksander.szczepanek@gmail.com>
2013-06-06 19:28:00 +0200

    Outline of the population class.

--- Genetics/Population.cs
+++ Genetics/Population.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace Genetics
 {
-    class Population
+    public class Population
     {
+        private List<Chromosome> _population;
+
+        private Chromosome _best;
+
+        public Population()
+        {
+
+        }
+
+        public Chromosome BestChromosome
+        {
+            get { return _best; }
+        }
+
+        public List<Chromosome> Chromosomes
+        {
+            get { return _population; }
+        }
+
+        public void Initiate()
+        {
+
+        }
+
+        public void Eval()
+        {
+
+        }
+
+        public void Select()
+        {
+
+        }
+
+        public void Cross()
+        {
+
+        }
+
+        public void Mutate()
+        {
+
+        }
+
+        public void CreateNewPopulation()
+        {
+
+        }
+
+        public bool CheckStopCondition()
+        {
+            return false;
+        }
     }
 }
