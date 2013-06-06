1ae097bcd3894b7213c52d5237979606d31708a9
Aleksander Szczepanek <aleksander.szczepanek@gmail.com>
2013-06-06 19:27:39 +0200

    Added separate group for evaluators. Defined test evaluator.

---
+++ Genetics/Evaluators/IEvaluator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetics.Evaluators
+{
+    /// <summary>
+    /// Interface for evaluators.
+    /// </summary>
+    public interface IEvaluator
+    {
+        /// <summary>
+        /// Evaluates chromosome.
+        /// </summary>
+        /// <param name="c">Chromosome to evaluate.</param>
+        /// <returns>Value of evaluated chromosome.</returns>
+        double Evaluate(Chromosome c);
+    }
+}
