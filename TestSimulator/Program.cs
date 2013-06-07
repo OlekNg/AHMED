ce49aa9c158ea32681f24e90a098d1983bca4f09
Andrzej Bartoszek <andrzej.tg.bartoszek@gmail.com>
2013-06-07 22:37:50 +0200

    Added references to TestProject

--- TestSimulator/Program.cs
+++ TestSimulator/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Simulation;
+using Structure;
 
 namespace TestSimulator
 {
@@ -16,6 +18,7 @@ namespace TestSimulator
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
