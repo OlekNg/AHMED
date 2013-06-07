af4b98ca7c34dc1cf1382b71b648781fd3dfa889
Aleksander Szczepanek <aleksander.szczepanek@gmail.com>
2013-06-07 21:05:01 +0200

    Added selector  exception class.
    Implemented roulette selector.

---
+++ Genetics/Selectors/RouletteSelector.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetics.Selectors
+{
+    public class RouletteSelector : ISelector
+    {
+        private Random randomizer;
+
+        public RouletteSelector()
+        {
+            randomizer = new Random();
+        }
+
+        public List<Chromosome> Select(List<Chromosome> population)
+        {
+            population.Sort();
+
+            // Distribution function.
+            List<double> F = new List<double>();
+
+            double temp = 0;
+
+            foreach (Chromosome c in population)
+                F.Add(temp += c.Value);
+
+            // Normalize ( F belongs to <0,1>
+            for (int i = 0; i < F.Count; i++)
+                F[i] /= temp;
+
+            // Drawing chromosomes.
+            List<Chromosome> result = new List<Chromosome>(population.Count);
+            for (int i = 0; i < F.Count; i++)
+            {
+                double number = randomizer.NextDouble();
+
+                // Binary search - later
+
+                Chromosome selected = null;
+
+                for (int j = 0; j < F.Count; j++)
+                {
+                    if (number < F[j])
+                    {
+                        selected = new Chromosome(population[j]);
+                        break;
+                    }
+                }
+
+                if (selected == null)
+                    selected = new Chromosome(population[population.Count - 1]);
+
+                result.Add(selected);
+            }
+
+            return result;
+        }
+    }
+}
