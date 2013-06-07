2b288cc6384663e270ecc4a58caaafe5c45489c8
Andrzej Bartoszek <andrzej.tg.bartoszek@gmail.com>
2013-06-07 19:41:08 +0200

    Empty project for testing purposes

---
+++ TestSimulator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSimulator
+{
+
+    //zebysmy sobie nie wchodzili w droge z testowaniem
+    //ja bede sie bawil na tym projekcie
+    //potem, oczywiscie, sie go usunie
+    //Andrzej
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
