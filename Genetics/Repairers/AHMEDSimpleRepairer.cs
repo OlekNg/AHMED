629b68c566931579b90aedf829da4bfa8ff2d69f
Aleksander Szczepanek <aleksander.szczepanek@gmail.com>
2013-06-09 13:06:42 +0200

    Added repairers outline.

---
+++ Genetics/Repairers/AHMEDSimpleRepairer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetics.Repairers
+{
+    public class AHMEDSimpleRepairer : IRepairer
+    {
+        public void RepairAndReplace(Chromosome c)
+        {
+            
+        }
+
+        public Chromosome RepairAndCreate(Chromosome c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
