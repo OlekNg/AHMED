ef14c04ae66eecc8c9c32c64580dbc059a033ef9
Aleksander Szczepanek <aleksander.szczepanek@gmail.com>
2013-06-11 19:59:32 +0200

    Updated test program.

--- AHMED/Program.cs
+++ AHMED/Program.cs
@@ -12,6 +12,7 @@ using Structure;
 using Simulation;
 using Genetics.Repairers;
 using System.Diagnostics;
+using SimpleVisualizer;
 
 namespace AHMED
 {
@@ -32,11 +33,11 @@ namespace AHMED
 
             Console.WriteLine();
 
+            VisualizerTest();
 
+            //OfficialTest();
 
-            OfficialTest();
-
-            Console.ReadKey();
+            //Console.ReadKey();
         }
 
         static void OfficialTest()
@@ -108,15 +109,15 @@ namespace AHMED
 
             Chromosome.MutationOperator = new ClassicMutation();
             Chromosome.CrossoverOperator = new OnePointCrossover();
-            Chromosome.Evaluator = new AHMEDEvaluator(sim);
-            //Generation.Selector = new TournamentSelector();
-            Generation.Selector = new RouletteSelector();
+            Chromosome.Evaluator = new AHMEDEvaluator(sim, 16, map);
+            Generation.Selector = new TournamentSelector();
+            //Generation.Selector = new RouletteSelector();
             //Generation.Repairer = new AHMEDSimpleRepairer(map);
             Generation.Repairer = new AHMEDAdvancedRepairer(map);
 
             Generation g = new Generation((int)map.Height * (int)map.Width * 2);
             g.MaxNumber = 500;
-            g.MutationProbability = 0.001;
+            g.MutationProbability = 0.01;
             g.CrossoverProbability = 0.75;
 
 //             Chromosome chr = new Chromosome("11010101010101" +
@@ -137,6 +138,7 @@ namespace AHMED
             s.Stop();
             Console.CursorTop += 6;
             Console.WriteLine("Best chromosome: {0} \n{1}", g.BestChromosome.Value, g.BestChromosome);
+            Console.WriteLine("Avg escape time: {0}", 35 - (g.BestChromosome.Value - 16));
             Console.WriteLine("Algorithm time: {0}ms", s.ElapsedMilliseconds);
         }
 
@@ -273,6 +275,83 @@ namespace AHMED
             g.Initiate(100);
             while (!g.Next()) { }
             Console.WriteLine("Best chromosome: {0} ({1})", g.BestChromosome.Value, g.BestChromosome);
+        }
+
+        static void VisualizerTest()
+        {
+            const uint w = 7;
+            const uint h = 5;
+            const uint capacity = 6;
+            const uint doorCapacity = 3;
+            const uint standardEff = 4;
+            BuildingMap map = new BuildingMap();
+            PeopleMap pmap = new PeopleMap();
+            Simulator sim = new Simulator();
+
+
+            map.Setup(w, h, standardEff);
+            //add floor
+            for (uint i = 0; i < h; ++i)
+                for (uint j = 0; j < w; ++j)
+                    map.SetFloor(i, j, capacity);
+            //add outer walls
+            for (uint i = 0; i < w; ++i)
+            {
+                map.SetWall(0, i, WallPosition.TOP);
+                map.SetWall(h, i, WallPosition.TOP);
+            }
+            for (uint i = 0; i < h; ++i)
+            {
+                map.SetWall(i, 0, WallPosition.LEFT);
+                map.SetWall(i, w, WallPosition.LEFT);
+            }
+            //add outer doors
+            map.SetDoor(3, 0, 10, WallPosition.LEFT);
+            map.SetDoor(2, 7, 5, WallPosition.LEFT);
+
+            //set inner walls
+            for (uint i = 0; i < 3; ++i)
+                map.SetWall(4, 2 + i, WallPosition.LEFT);
+            for (uint i = 0; i < 2; ++i)
+            {
+                map.SetWall(i, 2, WallPosition.LEFT);
+                map.SetWall(i, 4, WallPosition.LEFT);
+                map.SetWall(3 + i, 5, WallPosition.LEFT);
+                map.SetWall(3, 5 + i, WallPosition.TOP);
+            }
+            for (uint i = 0; i < w; ++i)
+                map.SetWall(2, i, WallPosition.TOP);
+            map.SetWall(4, 0, WallPosition.TOP);
+
+            //set inner doors
+            map.SetDoor(1, 4, doorCapacity, WallPosition.LEFT);
+            map.SetDoor(3, 5, doorCapacity, WallPosition.LEFT);
+            for (uint i = 0; i < 2; ++i)
+                map.SetDoor(2, 1 + i, doorCapacity, WallPosition.TOP);
+            for (uint i = 0; i < 3; ++i)
+                map.SetDoor(4, 1 + i, doorCapacity, WallPosition.TOP);
+
+            //add people group
+            pmap.People.Add(new PeopleGroup(0, 0, 3));
+            pmap.People.Add(new PeopleGroup(0, 3, 2));
+            pmap.People.Add(new PeopleGroup(0, 6, 6));
+            pmap.People.Add(new PeopleGroup(3, 6, 1));
+            pmap.People.Add(new PeopleGroup(4, 0, 2));
+            pmap.People.Add(new PeopleGroup(4, 2, 1));
+            pmap.People.Add(new PeopleGroup(4, 3, 1));
+
+            sim.SetupSimulator(map, pmap);
+            sim.MaximumTicks = 50;
+
+
+            Chromosome chr = new Chromosome("11010101010101" +
+                                            "11010100000000" +
+                                            "11011111111111" +
+                                            "00000000100000" + 
+                                            "11101010101010");
+
+            TestForm form = new TestForm(map, pmap, chr.Fenotype);
+            form.ShowDialog();
         }
     }
 }
--- SimpleVisualizer/TestForm.cs
+++ SimpleVisualizer/TestForm.cs
@@ -45,16 +45,18 @@ namespace SimpleVisualizer
 
             // Pens
             Pen wallPen = new Pen(Color.FromArgb(255, 0, 0, 255), 5);
-            Pen doorPen = new Pen(Color.Yellow, 5);
+            Pen doorPen = new Pen(Color.Orange, 5);
+            Pen pathPen = new Pen(Color.Green, 6);
 
             // Brushes
-            SolidBrush tileBrush = new SolidBrush(Color.FromArgb(255, 0, 0, 0));
+            SolidBrush tileBrush = new SolidBrush(Color.FromArgb(255, 200, 200, 200));
 
             // Draw tiles and walls/doors
             for (int i = 0; i < (int)_bmap.Height; i++)
             {
                 for (int j = 0; j < (int)_bmap.Width; j++)
                 {
+                    // Tile
                     if (_bmap.Floor[i][j] != null)
                         g.FillRectangle(tileBrush,
                                         wallSize + j * (tileSize + wallSize),
@@ -62,24 +64,31 @@ namespace SimpleVisualizer
                                         tileSize,
                                         tileSize);
 
+                    // Doors/walls
                     foreach (Direction d in Enum.GetValues(typeof(Direction)))
                     {
+                        // Nothing to draw.
+                        if (!_bmap.Floor[i][j].GetSide(d).Draw)
+                            continue;
+
                         // Prepare line coordinates.
                         Point p1 = new Point();
                         Point p2 = new Point();
 
-                        p1.X = j * (tileSize + wallSize) + (int)Math.Floor(wallSize / 2.0);
-                        p1.Y = i * (tileSize + wallSize) + (int)Math.Floor(wallSize / 2.0);
+                        p1.X = j * (tileSize + wallSize);
+                        p1.Y = i * (tileSize + wallSize);
 
                         switch (d)
                         {
                                 // Horizontal line (UP, DOWN)
                             case Direction.UP:
+                                p1.Y += (int)Math.Floor(wallSize / 2.0);
                                 p2.X = p1.X + tileSize + wallSize * 2;
                                 p2.Y = p1.Y;
                                 break;
 
                             case Direction.DOWN:
+                                p1.Y += (int)Math.Floor(wallSize / 2.0);
                                 p2.X = p1.X + tileSize + wallSize * 2;
                                 p2.Y = p1.Y;
                                 // Move line down
@@ -89,11 +98,13 @@ namespace SimpleVisualizer
 
                                 // Vertical line (LEFT, RIGHT)
                             case Direction.LEFT:
+                                p1.X += (int)Math.Floor(wallSize / 2.0);
                                 p2.X = p1.X;
                                 p2.Y = p1.Y + tileSize + wallSize * 2;
                                 break;
 
                             case Direction.RIGHT:
+                                p1.X += (int)Math.Floor(wallSize / 2.0);
                                 p2.X = p1.X;
                                 p2.Y = p1.Y + tileSize + wallSize * 2;
                                 // Move line right
@@ -106,8 +117,24 @@ namespace SimpleVisualizer
                         }
 
                         if (_bmap.Floor[i][j].GetSide(d).CanPassThrough)
-                            // Draw doors or nothing
+                        {
+                            // Draw doors
+                            // Doors has to be shorter.
+                            if (p1.Y == p2.Y)
+                            {
+                                // Horizontal line
+                                p1.X += wallSize;
+                                p2.X -= wallSize;
+                            }
+                            else
+                            {
+                                // Vertical line
+                                p1.Y += wallSize;
+                                p2.Y -= wallSize;
+                            }
+
                             g.DrawLine(doorPen, p1, p2);
+                        }
                         else
                             // Draw wall
                             g.DrawLine(wallPen, p1, p2);
@@ -115,7 +142,57 @@ namespace SimpleVisualizer
                 }
             }
 
-                uxImage.Image = b;
+            // Draw path from each people group
+            Point startPoint = new Point();
+            Point endPoint = new Point();
+            foreach (PeopleGroup group in _pmap.People)
+            {
+                int i = (int)group.Row;
+                int j = (int)group.Col;
+                // Start point of the escape path-line.
+                endPoint.X = startPoint.X = j * (tileSize + wallSize) + wallSize + tileSize / 2;
+                endPoint.Y = startPoint.Y = i * (tileSize + wallSize) + wallSize + tileSize / 2;
+
+                Console.WriteLine("Analyzing group at ({0}, {1})", i, j);
+
+                // While we are in the building - draw path.
+                while (i < _bmap.Height && j < _bmap.Width && i >= 0 && j >= 0)
+                {
+                    switch (_fenotype[i * (int)_bmap.Width + j])
+                    {
+                        case Direction.UP:
+                            endPoint.Y -= tileSize + wallSize;
+                            i--;
+                            break;
+
+                        case Direction.DOWN:
+                            endPoint.Y += tileSize + wallSize;
+                            i++;
+                            break;
+
+                        case Direction.LEFT:
+                            endPoint.X -= tileSize + wallSize;
+                            j--;
+                            break;
+
+                        case Direction.RIGHT:
+                            endPoint.X += tileSize + wallSize;
+                            j++;
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    g.DrawLine(pathPen, startPoint, endPoint);
+                    startPoint.X = endPoint.X;
+                    startPoint.Y = endPoint.Y;
+                }
+            }
+
+
+            // Show image.
+            uxImage.Image = b;
         }
     }
 }
