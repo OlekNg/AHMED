5da45ae8670b088560d7ea8625234a6a050ee5cb
Andrzej Bartoszek <andrzej.tg.bartoszek@gmail.com>
2013-06-11 19:55:06 +0200

    Working reading people map

--- Readers/XMLReader.cs
+++ Readers/XMLReader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Structure;
 
 namespace Readers
@@ -14,8 +15,24 @@ namespace Readers
         public PeopleMap ReadPeopleMap(string filepath)
         {
             PeopleMap result = new PeopleMap();
+            XDocument doc;
 
             _fileName = filepath;
+            try
+            {
+                doc = XDocument.Load(filepath);
+                var peopleGroups = doc.Descendants("group");
+                foreach (var group in peopleGroups)
+                {
+                    result.People.Add(new PeopleGroup(uint.Parse(group.Attribute("row").Value),
+                                                      uint.Parse(group.Attribute("col").Value),
+                                                      uint.Parse(group.Attribute("quantity").Value)));
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
 
 
             return result;
