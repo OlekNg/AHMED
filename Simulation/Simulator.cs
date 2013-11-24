78abc911c03c84cd5913db2855bf6540c430e8fe
Aleksander Szczepanek <aleksander.szczepanek@gmail.com>
2013-11-24 16:29:15 +0100

    Moved Direction enum from Structure to Common project. (step 2/2 with merging direction from Structure project).

--- Simulation/Simulator.cs
+++ Simulation/Simulator.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Structure;
 using Simulation.Exceptions;
+using Common.DataModel.Enums;
 
 namespace Simulation
 {
--- TestSimulator/Program.cs
+++ TestSimulator/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Simulation;
 using Structure;
+using Common.DataModel.Enums;
 
 namespace TestSimulator
 {
